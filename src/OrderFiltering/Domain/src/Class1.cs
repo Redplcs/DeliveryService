3829e85ab1da6f10c0e26eac6c3f47dd29513c34
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-23 01:11:51 +0300

    Add order filtering application domain source project and unit-testing project

---
+++ src/OrderFiltering/Domain/src/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace EffectiveMobile.DeliveryService.OrderFiltering.Domain;
+
+public class Class1
+{
+
+}
---
+++ src/OrderFiltering/Domain/tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace EffectiveMobile.DeliveryService.OrderFiltering.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
