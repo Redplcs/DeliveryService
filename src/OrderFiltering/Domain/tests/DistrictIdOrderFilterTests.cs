623123b6cb9089e059264ebb4f5f141926138da8
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-24 02:28:59 +0300

    Test that filter returns false for non-matching order

--- src/OrderFiltering/Domain/tests/DistrictIdOrderFilterTests.cs
+++ src/OrderFiltering/Domain/tests/DistrictIdOrderFilterTests.cs
@@ -13,4 +13,16 @@ public class DistrictIdOrderFilterTests
 
 		Assert.True(isValid);
 	}
+
+	[Fact]
+	public void ApplyFilter_OrderIsInvalid_ReturnsFalse()
+	{
+		var filterBy = DistrictId.Create();
+		var invalidOrder = Order.Create(OrderId.Create(), 1.0f, DistrictId.Create(), DateTime.UtcNow);
+		var filter = new DistrictIdOrderFilter(filterBy);
+
+		bool isValid = filter.ApplyFilter(invalidOrder);
+
+		Assert.False(isValid);
+	}
 }
