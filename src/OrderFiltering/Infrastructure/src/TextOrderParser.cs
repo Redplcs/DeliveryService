0a1ea4fef9e6553e294a9b84357832f3b22a041c
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-25 16:33:09 +0300

    Add order string parser

---
+++ src/OrderFiltering/Infrastructure/src/TextOrderParser.cs
@@ -0,0 +1,11 @@
+﻿using EffectiveMobile.DeliveryService.OrderFiltering.Domain;
+
+namespace EffectiveMobile.DeliveryService.OrderFiltering.Infrastructure;
+
+public class TextOrderParser
+{
+	public static Order Parse(string input)
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ src/OrderFiltering/Infrastructure/tests/TextOrderParserTests.cs
@@ -0,0 +1,26 @@
+﻿using EffectiveMobile.DeliveryService.OrderFiltering.Domain;
+using System.Globalization;
+
+namespace EffectiveMobile.DeliveryService.OrderFiltering.Infrastructure.Tests;
+
+public class TextOrderParserTests
+{
+	[Fact]
+	public void Parse_OrderSerializedToString_ReturnsDeserializedOrder()
+	{
+		var expectedOrderId = OrderId.Create();
+		var expectedWeight = 1.0f;
+		var expectedDeliveryDistrictId = DistrictId.Create();
+		var expectedDeliveryTime = DateTime.UtcNow;
+		var input = string.Format(CultureInfo.InvariantCulture, "{0},{1},{2},{3:yyyy-MM-dd HH:mm:ss}",
+			expectedOrderId, expectedWeight, expectedDeliveryDistrictId, expectedDeliveryTime);
+
+		var order = TextOrderParser.Parse(input);
+
+		Assert.NotNull(order);
+		Assert.Equal(expectedOrderId, order.Id);
+		Assert.Equal(expectedWeight, order.Weight);
+		Assert.Equal(expectedDeliveryDistrictId, order.DeliveryDistrictId);
+		Assert.Equal(expectedDeliveryTime, order.DeliveryTime);
+	}
+}
