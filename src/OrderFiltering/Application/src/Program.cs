03cb061aa3666bc6705994286b07ae72444dd288
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-27 15:53:21 +0300

    Filter provided orders if _cityDistrict parameter passed to app

--- src/OrderFiltering/Application/src/Program.cs
+++ src/OrderFiltering/Application/src/Program.cs
@@ -1,5 +1,33 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using EffectiveMobile.DeliveryService.OrderFiltering.Application.Commands;
+using EffectiveMobile.DeliveryService.OrderFiltering.Application.Services;
+using EffectiveMobile.DeliveryService.OrderFiltering.Domain;
+using EffectiveMobile.DeliveryService.OrderFiltering.Infrastructure.Orders;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var host = Host.CreateApplicationBuilder(args);
 
+AddFileTextOrderProviderIfHasParameter("_deliveryOrder");
+
+host.Services.AddSingleton<IFilterOrdersByDistrictCommand, FilterOrdersByDistrictCommand>();
+host.Services.AddHostedService<OrderFilteringService>();
+
 await host.Build().RunAsync();
+return;
+
+void AddFileTextOrderProviderIfHasParameter(string parameterName)
+{
+	var parameterSection = host.Configuration.GetSection(parameterName);
+
+	if (parameterSection.Exists())
+	{
+		var filePath = parameterSection.Value!;
+
+		host.Services.AddSingleton<IOrderProvider, TextFileOrderProvider>(_ =>
+		{
+			var fileStream = File.OpenRead(filePath);
+			return new TextFileOrderProvider(fileStream);
+		});
+	}
+}
---
+++ src/OrderFiltering/Application/src/Services/OrderFilteringService.cs
@@ -0,0 +1,52 @@
+﻿using EffectiveMobile.DeliveryService.OrderFiltering.Application.Commands;
+using EffectiveMobile.DeliveryService.OrderFiltering.Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace EffectiveMobile.DeliveryService.OrderFiltering.Application.Services;
+
+public class OrderFilteringService(
+	IFilterOrdersByDistrictCommand filterOrdersByDistrictCommand,
+	IConfiguration configuration,
+	ILogger<OrderFilteringService> logger) : BackgroundService
+{
+	protected override Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		var cityDistrict = configuration.GetSection("_cityDistrict");
+
+		if (cityDistrict.Exists())
+		{
+			logger.LogInformation("Filtering by district");
+
+			var districtGuid = Guid.Parse(cityDistrict.Value!);
+			filterOrdersByDistrictCommand.AddParameter(new DistrictId(districtGuid));
+			
+			var filteredOrders = filterOrdersByDistrictCommand.Execute();
+
+			LogFilteredOrders(filteredOrders);
+		}
+
+		// TODO: _firstDeliveryDateTime
+
+		return Task.CompletedTask;
+	}
+
+	private void LogFilteredOrders(IEnumerable<Order> orders)
+	{
+		var stringBuilder = new StringBuilder();
+		stringBuilder.AppendLine($"Filtered {orders.Count()} orders:");
+
+		foreach (var order in orders)
+		{
+			stringBuilder.AppendLine($"\t" +
+				$"OrderId = {order.Id}, " +
+				$"Weight = {order.Weight}, " +
+				$"DeliveryDistrictId = {order.DeliveryDistrictId}, " +
+				$"DeliveryTime = {order.DeliveryTime}");
+		}
+
+		logger.LogInformation(stringBuilder.ToString());
+	}
+}
