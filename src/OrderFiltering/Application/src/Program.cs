c5f1367d5283e6a1e2795f0f4eeb441f73e6369c
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-27 17:01:57 +0300

    Hide host configuration behind extensions

--- src/OrderFiltering/Application/src/Program.cs
+++ src/OrderFiltering/Application/src/Program.cs
@@ -1,70 +1,11 @@
-﻿using EffectiveMobile.DeliveryService.OrderFiltering.Application.Commands;
-using EffectiveMobile.DeliveryService.OrderFiltering.Application.Services;
-using EffectiveMobile.DeliveryService.OrderFiltering.Domain;
-using EffectiveMobile.DeliveryService.OrderFiltering.Infrastructure.Orders;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using EffectiveMobile.DeliveryService.OrderFiltering.Application.Extensions;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 var host = Host.CreateApplicationBuilder(args);
 
-AddFileTextOrderProviderIfHasParameter("_inputOrder");
-AddFileTextOrderSenderIfHasParameter("_deliveryOrder");
-AddFileTextLoggingIfHasParameter("_deliveryLog");
-
-host.Services.AddSingleton<IFilterOrdersByDistrictCommand, FilterOrdersByDistrictCommand>();
-host.Services.AddHostedService<OrderFilteringService>();
+host.AddFileTextOrderProviderIfHasParameter("_inputOrder");
+host.AddFileTextOrderSenderIfHasParameter("_deliveryOrder");
+host.AddFileTextLoggingIfHasParameter("_deliveryLog");
+host.AddOrderFilteringService();
 
 await host.Build().RunAsync();
-return;
-
-void AddFileTextOrderProviderIfHasParameter(string parameterName)
-{
-	var parameterSection = host.Configuration.GetSection(parameterName);
-
-	if (parameterSection.Exists())
-	{
-		var filePath = parameterSection.Value!;
-
-		host.Services.AddSingleton<IOrderProvider, TextFileOrderProvider>(_ =>
-		{
-			var fileStream = File.OpenRead(filePath);
-			return new TextFileOrderProvider(fileStream);
-		});
-	}
-}
-
-void AddFileTextOrderSenderIfHasParameter(string parameterName)
-{
-	var parameterSection = host.Configuration.GetSection(parameterName);
-
-	if (parameterSection.Exists())
-	{
-		var filePath = parameterSection.Value!;
-
-		host.Services.AddSingleton<IOrderSender, TextFileOrderSender>(_ =>
-		{
-			var fileStream = File.OpenWrite(filePath);
-			return new TextFileOrderSender(fileStream);
-		});
-	}
-}
-
-void AddFileTextLoggingIfHasParameter(string parameterName)
-{
-	var parameterSection = host.Configuration.GetSection(parameterName);
-
-	if (parameterSection.Exists())
-	{
-		var filePath = parameterSection.Value!;
-
-		host.Services.AddLogging(logging =>
-		{
-			logging.AddFile(options =>
-			{
-				options.Path = filePath;
-			});
-		});
-	}
-}
