08b076d3080f92d614f372692d26433319739b66
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-12-17 16:27:24 +0300

    Add validation attributes for order entity

--- src/OrderManagementService/Domain/src/Order.cs
+++ src/OrderManagementService/Domain/src/Order.cs
@@ -1,4 +1,6 @@
-﻿namespace EffectiveMobile.DeliveryService.OrderManagementService.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EffectiveMobile.DeliveryService.OrderManagementService.Domain;
 
 public class Order
 {
@@ -7,8 +9,15 @@ public class Order
 		Id = id;
 	}
 
+	[Required, Key]
 	public Guid Id { get; }
+
+	[Required, Range(0.01, 1000)]
 	public double Weight { get; set; }
+
+	[Required]
 	public Guid DeliveryDistrictId { get; set; }
+
+	[Required, DataType(DataType.DateTime)]
 	public DateTimeOffset DeliveryTime { get; set; }
 }
