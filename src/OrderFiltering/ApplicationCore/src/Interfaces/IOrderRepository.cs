91c6add7c133e062bcfc2fdc4003319961db4466
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-12-18 12:10:59 +0300

    Add repository interface

---
+++ src/OrderFiltering/ApplicationCore/src/Interfaces/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using EffectiveMobile.DeliveryService.OrderFiltering.ApplicationCore.Entities;
+
+namespace EffectiveMobile.DeliveryService.OrderFiltering.ApplicationCore.Interfaces;
+
+public interface IOrderRepository
+{
+	void Add(Order order);
+	Order GetOrder(Guid id);
+	IEnumerable<Order> GetOrders();
+	void Update(Order order);
+	void Remove(Order order);
+	void Save();
+}
